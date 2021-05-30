ad135c2a3591a2ed9e323da24290fb6bb6a54952
BeNcH <slava.morzhakov@mail.ru>
2021-05-30 22:51:33 +0300

    Добавлена отчетность. Рабочая!

---
+++ Lab1/Controllers/CreateExelController.cs
@@ -0,0 +1,69 @@
+﻿using Lab1.Models;
+using Lab1.Models.Security;
+using Microsoft.AspNet.Identity;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class CreateExelController : Controller
+    {
+        public FileResult GetUsersReport()
+        {
+            // Путь к файлу с шаблоном
+            string file_path_template =
+           Server.MapPath("~/Content/Reports/report_template.xlsx");
+            FileInfo fi = new FileInfo(file_path_template);
+            //Путь к файлу с результатом
+            string file_path = Server.MapPath("~/Content/Reports/report.xlsx");
+            FileInfo fi_report = new FileInfo(file_path);
+            //будем использовть библитотеку не для коммерческого использования
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            //открываем файл с шаблоном
+            using (ExcelPackage excelPackage = new ExcelPackage(fi))
+            {
+                //устанавливаем поля документа
+                excelPackage.Workbook.Properties.Author = "Моржаков В.С.";
+                excelPackage.Workbook.Properties.Title = "Список пользователей системы";
+                excelPackage.Workbook.Properties.Subject = "Пользователи системы";
+                excelPackage.Workbook.Properties.Created = DateTime.Now;
+
+                //плучаем лист по имени.
+                ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets["Лист1"];
+                //получаем списко пользователей и в цикле заполняем лист данными
+                int startLine = 3;
+                RoomsDBContext db = new RoomsDBContext();
+
+                foreach (var item in db.NewStudents.ToList())
+                {
+                    worksheet.Cells[startLine, 1].Value = startLine - 2;
+                    worksheet.Cells[startLine, 2].Value = item.ID;
+                    worksheet.Cells[startLine, 3].Value = item.Name;
+                    worksheet.Cells[startLine, 4].Value = item.Surname;
+                    worksheet.Cells[startLine, 5].Value = item.Otch;
+                    worksheet.Cells[startLine, 6].Value = item.PasportSer;
+                    worksheet.Cells[startLine, 7].Value = item.PasportNumber;
+                    worksheet.Cells[startLine, 8].Value = item.Sex;
+                    worksheet.Cells[startLine, 9].Value = item.Birthday;
+                    worksheet.Cells[startLine, 10].Value = item.Town;
+                    startLine++;
+                }
+
+
+                //созраняем в новое место
+                excelPackage.SaveAs(fi_report);
+            }
+            // Тип файла - content-type
+            string file_type = "application/vnd.openxmlformatsofficedocument.spreadsheetml.sheet";
+            // Имя файла - необязательно
+            string file_name = "report.xlsx";
+            return File(file_path, file_type, file_name);
+        }
+        
+    }
+}
